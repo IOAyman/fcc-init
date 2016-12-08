d4ef38b3714daf3f4fcb22d45477aefae1199ab4
Ayman Nedjmeddine <theycallmethedr@gmail.com>
2016-12-09 00:07:36 +0100

    Override mongoose's deprecated promises lib

--- go.js
+++ go.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const fs = require('fs')
-const routes = require('./routes')
 
 
 // setup env
@@ -17,13 +16,14 @@ const app = express()
 
 // setup db
 const { DB_HOST, DB_PORT, DB_NAME, DB_USER, DB_USER_PASS } = process.env
+mongoose.Promise = global.Promise
 mongoose.connect(`mongodb://${DB_USER}:${DB_USER_PASS}@${DB_HOST}:${DB_PORT}/${DB_NAME}`)
 mongoose.connection.on('connected', () => console.info('DB connected!'))
 mongoose.connection.on('error', console.error)
 
 
 // routes
-app.use('/', routes)
+app.use('/', require('./routes'))
 
 // about this project
 app.all('*', (req, res, next) => res.end(`
